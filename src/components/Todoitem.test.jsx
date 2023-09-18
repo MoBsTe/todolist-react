da4789874c506f46c8f3c847397be84ea050d378
MoBsTe <valery.lobach@gail.com>
2023-09-18 03:07:59 +0200

    create react todo list

---
+++ src/components/Todoitem.test.jsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Todoitem from './Todoitem';
+const item = { id: 1, text: 'testText', completed: false, important: false }
+
+describe('TodoItem component', () => {
+    it('TodoItem render', () => {
+        render(<Todoitem item={item} />);
+
+        expect(screen.getByText(/testText/)).toBeInTheDocument();
+    });
+
+    it('TodoItem renders without data', () => {
+        render(<Todoitem />);
+
+        expect(screen.queryByRole('list')).toBeNull();
+
+    });
+});
